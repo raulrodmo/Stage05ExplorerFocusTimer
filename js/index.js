7cd56e645079cdca98b6266aefb19da1cdc6c800
raulrodmo <raulrm.dev@gmail.com>
2023-02-25 19:06:13 -0300

    update and modularize the js

--- js/index.js
+++ js/index.js
@@ -1,35 +1,36 @@
+import resetControls from "./controls.js"
+import "./timer.js"
+
+
 let buttonPlay = document.querySelector('.play')
 let buttonPause = document.querySelector('.pause')
 let buttonStop = document.querySelector('.stop')
 let buttonSet = document.querySelector('.set')
 let buttonUnmute = document.querySelector('.unmute')
 let buttonMute = document.querySelector('.mute')
-let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-
-
-
-
-const form = document.querySelector('form')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeout
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  coutdown()
 })
 
 buttonPause.addEventListener('click', function () {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
+  clearTimeout(timerTimeout)
 })
 
 buttonStop.addEventListener('click', function(){
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+  resetControls()
+  resetTimer()
 })
 
 buttonUnmute.addEventListener('click', function () {
@@ -43,6 +44,12 @@ buttonMute.addEventListener('click', function () {
 })
 
 buttonSet.addEventListener('click', function () {
-  minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent=minutes
+  let newMinutes = prompt('Quantos minutos?')
+  if (newMinutes){
+    resetTimer()
+    return
+  }
+
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
 })
